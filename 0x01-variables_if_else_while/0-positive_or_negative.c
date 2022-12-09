96ca662023e985fe048538b805e7236c36b11f88
Faustin Mburu <faustinwesh2@gmail.com>
2022-12-09 13:12:28 -0800

    Task 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - main function to generate a random number
+ * main function to generate a random number
  *
  * Return: Always 0 (Success)
  *
@@ -17,10 +17,8 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-	
 	{
 		printf("%d is positive\n", n);
-	
 	}
 	else if (n == 0)
 	{
